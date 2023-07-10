47d819e5a64771007721a3a8992fd04c230e3291
Nick Fenger <nfenger@pps.net>
2023-07-09 19:40:11 -0700

    Swapped relay pins 4 & 2

--- src/relaycontrol.cpp
+++ src/relaycontrol.cpp
@@ -3,9 +3,9 @@
 #include "relaycontrol.h"
 
 #define RELAY1PIN 4 //veggie
-#define RELAY2PIN 5 //tomatoes
+#define RELAY2PIN 7 //tomatoes
 #define RELAY3PIN 6 //front yard
-#define RELAY4PIN 7 //potted flowers
+#define RELAY4PIN 5 //potted flowers
 
 
 
